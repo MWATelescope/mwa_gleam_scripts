9de8ca06b39e46d141ee06182992ab55b8b0413c
Danny Jacobs <daniel.c.jacobs@asu.edu>
2012-06-26 15:17:32 -0700

    moving around files src->mwapy

--- src/catalog/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This module supports cataloging from within CASA or in python <=2.7
-
-Dependencies:
-AIPY 1.0
-pyephem 3.7.4.1
-"""
-from generic_catalog import *
--- src/pb/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This module defines a python interface into a simple model of the primary beam.
-"""
-import measured_beamformer,mwapb
-
