f6bd9bbc843eb1426e130b3ac504b19c9af3b087
Danny Jacobs <daniel.c.jacobs@asu.edu>
2012-09-12 14:05:39 -0700

    fixed cd error in clean_MWA_Tools.sh

--- clean_MWA_Tools.sh
+++ clean_MWA_Tools.sh
@@ -3,6 +3,10 @@
 #this is key to making sure systems specific things don't end up in the repo!
 
 cd CONV2UVFITS; make clean
+cd ..
 cd casatasks; make clean
+cd ..
 cd build_lfiles; make clean
+cd ..
 cd cfitsio; make clean
+cd ..
