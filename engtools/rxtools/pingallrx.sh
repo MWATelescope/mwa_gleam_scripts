7cd53fc9ee816a898e73f0988833ee14693e336a
Dave Emrich <d.emrich@curtin.edu.au>
2013-02-21 17:21:13 +0800

    Fixed pingall to work with eiethe icmp_seq or icmp_req messages

--- engtools/rxtools/pingallrx.sh
+++ engtools/rxtools/pingallrx.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 for i in {1..9}; do
- ping -c 1 rec0$i | grep icmp_seq
+ ping -c 1 rec0$i | grep icmp_
 done
 for i in {0..6}; do
- ping -c 1 rec1$i | grep icmp_seq
+ ping -c 1 rec1$i | grep icmp_
 done
