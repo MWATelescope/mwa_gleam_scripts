00328191cbda45dfd8c9dda058da6e63df989b3a
Dave Emrich <d.emrich@curtin.edu.au>
2013-02-22 11:37:50 +0800

    Made message in poweroffallrx.sh more accurate and informative

--- engtools/rxtools/poweroffallrx.sh
+++ engtools/rxtools/poweroffallrx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for i in {1..9}; do
- echo testing rec0$i
+ echo Shutting down DC rails on rec0$i
  ssh mwa@rec0$i bin/receiver_power off
 done
 for i in {0..6}; do
- echo testing rec1$i
+ echo Shutting down DC rails on rec1$i
  ssh mwa@rec1$i bin/receiver_power off
 done
