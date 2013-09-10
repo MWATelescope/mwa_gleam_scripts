66fde0c92ddbd8604a6c49babe6f1397fb7bcb69
Aaron <aaronew@mit.edu>
2013-09-10 13:28:09 -0400

    beam stuff adde

---
+++ eorlive/generate_beam_image.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import subprocess as sp
+import psycopg2,os,glob
+from datetime import datetime
+
+
+
+if __name__=="__main__":
+    #get the last command in mwa_setting
+    eorconn = psycopg2.connect(database='mwa',host='eor-db.mit.edu',user='mwa',password='BowTie')
+    cur = eorconn.cursor()
+    cur.execute('select starttime from mwa_setting where starttime < gpsnow() order by starttime desc limit 1')
+    obsid=cur.fetchall()[0][0]
+    #get current observation
+    sp.call('/csr/mwa/python/mwa_git/mwatools_setup/bin/get_observation_info.py -g '+str(obsid)+' -i',shell=True)
+    #get latest png
+    #remove all images
+    pngfile = sorted(glob.glob('/nfs/blank/h4215/aaronew/MWA_Tools/eorlive/*.png'),key=os.path.getmtime)
+    pngfile = pngfile[len(pngfile)-1]
+    f=open('/nfs/blank/h4215/aaronew/MWA_Tools/eorlive/current_beam.log','a')
+    nowtime=datetime.now()
+    f.write(nowtime.isoformat(' ')+' successfully executed')
+    f.close()
+    exit(0)
+    
