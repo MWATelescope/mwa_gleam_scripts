128214cbf9622f802e55f2cfc0302bc1168ec6a6
charlieko <charlesthedark@gmail.com>
2014-07-06 21:41:01 -0700

    log in interface

--- eorlive_v2/eorlive/controllers/index.py
+++ eorlive_v2/eorlive/controllers/index.py
@@ -3,4 +3,8 @@ from flask import render_template, request
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
+@app.route('/login')
+def index_login():
+  return render_template('login.html')
--- eorlive_v2/eorlive/static/js/eor.js
+++ eorlive_v2/eorlive/static/js/eor.js
@@ -2,6 +2,8 @@ var EoR = {};
 
 EoR.init = function(){
 
+  $.ajaxSetup({type:"json"});
+
   // Handle hash change event and navbar component updates
   function hashChanged(){
     $(".content_container").hide();
@@ -13,11 +15,52 @@ EoR.init = function(){
   $(window).on('hashchange', hashChanged);
   hashChanged();
 
-  // Initiate content boxes
-  EoR.clock.init(); // Clocks widget
-  EoR.google.init(); // Logs and Graphs based on Google APIs
+  EoR.check_user(function(){
+    $(".navbar .logging_in_msg").remove();
+    $(".navbar .logged_in_msg")
+      .append("Hello " + EoR.current_user.name + " (")
+      .append($("<a/>").attr("href", "#").text("Log out").click(EoR.logout))
+      .append(")");
+    // Initiate content boxes
+    EoR.clock.init(); // Clocks widget
+    EoR.google.init(); // Logs and Graphs based on Google APIs
+  });
 };
 
 EoR.create_loading = function(){
   return $("<img>").addClass("loading").attr("src", STATIC_PATH+"/img/loading.gif");
-}
+};
+
+
+EoR.check_user = function(cb){
+  $.ajax({
+    method: "GET",
+    url: "/api/current_user",
+    type: "json",
+    success: function(data){
+      EoR.current_user = data;
+      if(cb) cb();
+    },
+    error: function(xhr, status, err){
+      window.location = "/login";
+    }
+  });
+};
+
+EoR.logout = function(e){
+  $.ajax({
+    method: "POST",
+    url: "/api/logout",
+    type: "json",
+    success: function(data){
+      window.location = "/login";
+    },
+    error: function(xhr, status, err){
+      alert("something went wrong...");
+    }
+  });
+};
+
+EoR.render_user_info = function(){
+
+};
