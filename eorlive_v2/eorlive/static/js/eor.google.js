6534e295a3cb2aa33a56c8768de78db84ce8f0b1
charlieko <charlesthedark@gmail.com>
2014-07-08 17:29:22 -0700

    add google fusion tables

--- eorlive_v2/eorlive/static/js/eor.google.js
+++ eorlive_v2/eorlive/static/js/eor.google.js
@@ -2,6 +2,26 @@ EoR.google = {};
 
 EoR.google.SPREADSHEET = "https://docs.google.com/a/mindbloom.com/spreadsheet/ccc?key=0Ate0sqkDCwhydEpHMnlHckFSRjRhaE5IdHFUcDd2enc#gid=0";
 
+// What to render for the google fusion table iframes.
+// array of [source, title, width, height]
+EoR.google.FT_SOURCES = [
+  [
+    "https://www.google.com/fusiontables/embedviz?containerId=googft-gviz-canvas&viz=GVIZ&t=LINE_AGGREGATE&isXyPlot=true&bsize=0&q=select+col0%2C+col1%2C+col5%2C+col4%2C+col25+from+1UspWE7G7ccKncYTUbEkpSaSitomsN-7vLkNdx-I&qrs=+where+col0+%3E%3D+&qre=+and+col0+%3C%3D+&qe=+order+by+col0+asc&uiversion=2&gco_forceIFrame=true&gco_hasLabelsColumn=true&width=<width>&height=<height>",
+    "Hours of Observing",
+    1000, 600
+  ],
+  [
+    "https://www.google.com/fusiontables/embedviz?containerId=gviz_canvas&viz=GVIZ&t=LINE_AGGREGATE&isXyPlot=true&bsize=0&q=select+col0%2C+col24+from+1UspWE7G7ccKncYTUbEkpSaSitomsN-7vLkNdx-I&qrs=+where+col0+%3E%3D+&qre=+and+col0+%3C%3D+&qe=+order+by+col0+asc&uiversion=2&gco_forceIFrame=true&gco_hasLabelsColumn=true&width=<width>&height=<height>",
+    "", 1000, 600
+  ]
+  /*
+  [
+    "https://www.google.com/fusiontables/embedviz?containerId=gviz_canvas&viz=GVIZ&t=LINE_AGGREGATE&isXyPlot=true&bsize=0&q=select+col0%2C+col7%2C+col6%2C+col8%2C+col9%2C+col10%2C+col11%2C+col12%2C+col13%2C+col14%2C+col15%2C+col16%2C+col18%2C+col19%2C+col20%2C+col21+from+1UspWE7G7ccKncYTUbEkpSaSitomsN-7vLkNdx-I&qrs=+where+col0+%3E%3D+&qre=+and+col0+%3C%3D+&qe=+order+by+col0+asc&uiversion=2&gco_forceIFrame=true&gco_hasLabelsColumn=true&width=<width>&height=<height>",
+    "Failure Rate", 1000, 600
+  ]
+  */
+];
+
 function prepend_zero(d){
   if (d<10) return "0"+d;
   return d;
@@ -29,6 +49,22 @@ EoR.google.create_sheetrock_options = function(loading_indicator){
 EoR.google.create_log_spreadsheet = function(opt){
   return table = $("<table/>").addClass("gs_table table table-striped")
     .sheetrock(opt);
+};
+
+EoR.google.create_fb_iframe = function(src, title, width, height){
+  src = src.replace("<width>", width).replace("<height>", height);
+
+  var iframe = $("<iframe/>")
+    .attr("src", src)
+    .attr("width", width)
+    .attr("height", height)
+    .attr("scrolling", "no")
+    .attr("frameborder", "no");
+
+  return $("<div/>")
+    .addClass("iframe_container")
+    .append("<h1>"+title+"</h1>")
+    .append(iframe);
 };
 
 EoR.google.init = function(){
@@ -62,4 +98,12 @@ EoR.google.init = function(){
 
   $("#logs .table_container")
     .append(EoR.google.create_log_spreadsheet(EoR.google.create_sheetrock_options(loading_indicator)));
+
+  // Fusion Tables
+  // Render them in overview for now
+  $.each(EoR.google.FT_SOURCES, function(i,v){
+    $("#home")
+      .append(EoR.google.create_fb_iframe(v[0], v[1], v[2], v[3]));
+  });
+
 };
