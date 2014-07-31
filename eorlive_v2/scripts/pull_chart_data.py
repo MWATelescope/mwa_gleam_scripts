b797d2092bccedb0316c220341e1cc366b74bda9
charlieko <charlesthedark@gmail.com>
2014-07-30 20:27:08 -0700

    pull_chart_data script update - vastly more efficient now

--- eorlive_v2/scripts/pull_chart_data.py
+++ eorlive_v2/scripts/pull_chart_data.py
@@ -14,10 +14,13 @@ mwa_conn = None
 local_conn = None
 profiling_mark = None
 
+TESTING = True
+
 cur_d = os.path.dirname(__file__)
 
 def write_to_log(msg):
   print msg
+  if TESTING: return
   f=open(os.path.join(cur_d, PATH_TO_LOG),'a')
   try:
     f.write(msg)
@@ -78,24 +81,69 @@ def update():
       LEFT OUTER JOIN data_files ON mwa_setting.starttime = data_files.observation_num
       WHERE projectid='G0009'
       GROUP BY starttime, stoptime) as subq
-    WHERE subq.files > 0''').fetchall()
+    WHERE subq.files > 0
+    ORDER BY subq.starttime ASC''')
 
-  write_to_log("mwa_setting_rows-that-have-more-than-one-file query ran in %f seconds" %profile())
+  write_to_log("mwa_setting_rows query ran in %f seconds" %profile())
 
-  write_to_log("preparing for the dreadful n queries where n = %d" %len(mwa_setting_rows) )
+  ngas_files_rows = send_ngas_query(
+  '''
+  SELECT file_id FROM ngas_files
+  ORDER BY file_id ASC
+  '''
+  )
+
+  write_to_log("ngas_files_rows query ran in %f seconds" %profile())
+
+  write_to_log("preparing for the giant interation where mwa_setting_rows = %d and ngas_files_rows = %d"
+    % (mwa_setting_rows.rowcount, ngas_files_rows.rowcount ))
   i = 0
   total_data_hours = 0
+  no_more_ngas_files = False
+  match = 0
+  no_match = 0
   for row in mwa_setting_rows:
     #if i >= 10: break
-    obsid=str(int(row[0]))
-    num_ngas_files = send_ngas_query('''
-      SELECT COUNT(DISTINCT file_id) FROM ngas_files WHERE file_id LIKE '%s%%'
-    ''' %obsid).fetchone()[0]
+    obsid = int(row[0])
+    obsid_str=str(obsid)
+    num_ngas_files = 0
+
+    while(True):
+      ngas_file = ngas_files_rows.fetchone()
+      if not ngas_file:
+        no_more_ngas_files = True
+        print "no_more_ngas_files set to True"
+        break;
+
+      ngas_file_id = ngas_file[0]
+
+      try:
+        ngas_file_obsid = int(ngas_file_id.split("_")[0])
+      except ValueError as e:
+        ngas_file_obsid = 0 # so it can be just fast forwarded
+
+      if ngas_file_id.startswith(obsid_str):
+        num_ngas_files += 1
+        match += 1
+      else:
+
+        # fast forward if ngas_file_obsid is smaller - for any normal situation it should be equal or greater
+        if ngas_file_obsid < obsid: continue
+
+        # if not move the cursor one step back and break so it fetches the same row on the next loop
+        ngas_files_rows.scroll(-1)
+        no_match += 1
+        break;
+
     num_mit_files = row[2]
     total_data_hours += (float(num_ngas_files) / float(num_mit_files)) * (row[1] - row[0]) / 3600.
     i += 1
 
-  write_to_log("dreadful n queries ran in %f seconds" %profile())
+    if no_more_ngas_files: break;
+
+  print "match = %d no_match = %d" %(match, no_match)
+
+  write_to_log("The giant iteration ran in %f seconds" %profile())
 
   # total_data_hours = float (send_eor_query('''
   #   SELECT SUM(subq.stoptime-subq.starttime)
@@ -117,16 +165,16 @@ def update():
   write_to_log("total_uvvits_hours query ran in %f seconds" %profile())
 
   # Data transfer rate
-  data_transfer_rate = 0
-  last_query_time = send_local_query(
-    '''
-    SELECT created_date FROM graph_data ORDER BY created_date DESC LIMIT 1
-    ''').fetchone()
-  if last_query_time is not None:
-    last_query_time = last_query_time[0]
-    time_delta = datetime.now() - last_query_time
-    last_query_data_hours = send_local_query("SELECT hours_with_data FROM graph_data ORDER BY created_date DESC LIMIT 1").fetchone()[0]
-    data_transfer_rate = (total_data_hours - last_query_data_hours) / (time_delta.total_seconds() / 3600.)
+  data_transfer_rate = send_ngas_query(
+  """
+  select sum(uncompressed_file_size)/date_part('epoch', to_timestamp('%(now)s','YYYY-MM-DD"T"HH24:MI:SS.MS')- to_timestamp(min(ingestion_date),'YYYY-MM-DD"T"HH24:MI:SS.MS') )/(1024^2) as "data_transfer_rate"
+from ngas_files
+where ingestion_date between to_char(to_timestamp('%(now)s','YYYY-MM-DD"T"HH24:MI:SS.MS')- interval '24 hours','YYYY-MM-DD"T"HH24:MI:SS.MS') and '%(now)s';
+  """ % {"now": datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000')}
+  ).fetchone()[0] or 0
+  data_transfer_rate = float(data_transfer_rate)
+
+  write_to_log("data_transfer_rate query ran in %f seconds" %profile())
 
   write_to_log("\nTotal Scheduled Hours = %.6f" %total_sch_hours)
   write_to_log("Total Observed Hours = %.6f" %total_obs_hours)
@@ -134,7 +182,8 @@ def update():
   write_to_log("Total Hours that have uvfits data = %.6f" %total_uvfits_hours)
   write_to_log("Data transfer rate = %.6f" %data_transfer_rate)
 
-  # TODO insert data_transfer_rate
+  if TESTING: return exit(0)
+
   send_local_query("""
     INSERT INTO graph_data (hours_scheduled, hours_observed, hours_with_data, hours_with_uvfits, data_transfer_rate)
     VALUES (%f, %f, %f, %f, %f)
