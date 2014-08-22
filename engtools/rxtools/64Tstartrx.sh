df3de575c1fda8c25fd6d3d6e580ed826f3488da
Randall Wayth <r.wayth@curtin.edu.au>
2014-08-22 13:04:34 +0800

    removed obsolete eng tools

--- engtools/rxtools/64Tstartrx.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-startrxlist.sh 04 12 03 05 06 11 13 14
--- engtools/rxtools/armallrx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in {1..9}; do
- echo testing rec0$i
- ssh mwa@rec0$i bin/arm_clock
-done
-for i in {0..6}; do
- echo testing rec1$i
- ssh mwa@rec1$i bin/arm_clock
-done
