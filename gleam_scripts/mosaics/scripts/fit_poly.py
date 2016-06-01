fa26e92a8e87f2b4bdb6bdc03553f3947bf33211
Natasha Hurley-Walker <nhw@icrar.org>
2016-06-01 14:07:08 +0800

    Updated XX:YY flux ratio fitting script

--- gleam_scripts/mosaics/scripts/fit_poly.py
+++ gleam_scripts/mosaics/scripts/fit_poly.py
@@ -6,7 +6,7 @@ import os
 import re
 import glob
 
-import astropy.io.fits as pyfits
+import astropy.io.fits as fits
 from astropy import wcs
 
 #tables and votables
@@ -20,14 +20,15 @@ parser.add_option('--plot',action="store_true",dest="make_plots",default=False,
                   help="Make fit plots? (default = False)")
 parser.add_option('--rescale',action="store_true",dest="do_rescale",default=False,
                   help="Generate rescaled fits files? (default = False)")
-parser.add_option('--order',dest="poly_order",default=3,type=int,
-                  help="Set the order of the polynomial fit. (default = 3)")
+parser.add_option('--order',dest="poly_order",default=5,type=int,
+                  help="Set the order of the polynomial fit. (default = 5)")
+parser.add_option('--write',action="store_true",dest="write_coefficients",default=False,
+                    help="Write coefficients to file? (default = False)")
 (options, args) = parser.parse_args()
 
-tables = sorted(glob.glob('*_XY*.vot'))
+tables = sorted(glob.glob('*_XY*_comp.vot'))
 
 if options.make_plots:
-
     import matplotlib as mpl
     mpl.use('Agg') # So does not use display
     import matplotlib.pyplot as pyplot
@@ -57,18 +58,27 @@ if options.make_plots:
 
 for tablefile in tables:
 
+    input_root=tablefile.replace("_comp.vot","")
+
     title=tablefile.split('_')[1]+'_'+tablefile.split('_')[2]
     table = parse_single_table(tablefile)
     data = table.array
 
     x=data['dec_X']
-    y=data['peak_flux_X']/data['peak_flux_Y']
+    y=data['peak_flux_Y']/data['peak_flux_X']
     w=data['peak_flux_X']/data['local_rms_X']
 
     N=len(data['dec_X'])
 
     P,res,rank,sv,cond = np.ma.polyfit(np.ma.array(x),np.ma.array(y),options.poly_order,full=True,w=w)
     fitmodel = np.poly1d(P)
+# Remove outliers
+    modsub = np.abs(y - fitmodel(x))
+    indices = np.where(modsub<0.2)
+
+    P,res,rank,sv,cond = np.ma.polyfit(np.ma.array(x[indices]),np.ma.array(y[indices]),options.poly_order,full=True,w=w[indices])
+    fitmodel = np.poly1d(P)
+
 
     print "fit parameters:"
     print P
@@ -97,16 +107,17 @@ for tablefile in tables:
         plotnum+=1
 
     if options.do_rescale:
-        Yfits=re.sub('_XY_r-1.0_comp.vot','_YY_r-1.0.fits',tablefile)
-        newYfits=re.sub('_YY_r-1.0.fits','_YY_r-1.0_resc.fits',Yfits)
+        Xfits=re.sub('_XY_r','_XX_r',tablefile)
+        Xfits=re.sub('_comp.vot','.fits',Xfits)
+        newXfits=re.sub('.0.fits','.0_resc.fits',Xfits)
 
-        if os.path.isfile(newYfits):
-            os.remove(newYfits)
+        if os.path.isfile(newXfits):
+            os.remove(newXfits)
 
-    # Modify the YY fits file to produce a new version
-    # YYcorr=a*(Dec)^3 + b*(Dec)^2 + c*Dec + d
+    # Modify the XX fits file to produce a new version
+    # XXcorr=a*(Dec)^3 + b*(Dec)^2 + c*Dec + d
 
-        hdu_in=pyfits.open(Yfits)
+        hdu_in=fits.open(Xfits)
     # Fixing header problem. Removing third axis.
         try:
             test=hdu_in[0].header['CRPIX3']
@@ -134,13 +145,38 @@ for tablefile in tables:
             indexes[i*j:(i+1)*j] = idx
     #put ALL the pixles into our vectorized functions and minimised our overheads
         ra,dec = w.wcs_pix2world(indexes,1).transpose()
-        YYcorr=(P[0]*pow(dec,3)+P[1]*pow(dec,2)+P[2]*(dec)+P[3])
-        reshapedYYcorr=YYcorr.reshape(hdu_in[0].data.shape[0],hdu_in[0].data.shape[1])
-        hdu_in[0].data=np.array(reshapedYYcorr*hdu_in[0].data,dtype=np.float32)
-        hdu_in.writeto(newYfits)
+        XXcorr=np.zeros(dec.shape)
+        for i in range(0,options.poly_order+1):
+            XXcorr+=P[i]*pow(dec,options.poly_order-i)
+#        XXcorr=(P[0]*pow(dec,3)+P[1]*pow(dec,2)+P[2]*(dec)+P[3])
+        reshapedXXcorr=XXcorr.reshape(hdu_in[0].data.shape[0],hdu_in[0].data.shape[1])
+        hdu_in[0].data=np.array(reshapedXXcorr*hdu_in[0].data,dtype=np.float32)
+        hdu_in.writeto(newXfits)
         hdu_in.close()
+
+if options.write_coefficients:
+    outcoeff=input_root+'_xxyy_coefficients.fits'
+    print "#----------------------------------------------------------#"
+    print 'Saving correction to '+outcoeff
+
+    fit_name = ['quintic']
+    col1 = fits.Column(name='z', format = 'E', array = [P[5]])
+    col2 = fits.Column(name='y', format = 'E', array = [P[4]])
+    col3 = fits.Column(name='x', format = 'E', array = [P[3]])
+    col4 = fits.Column(name='w', format = 'E', array = [P[2]])
+    col5 = fits.Column(name='v', format = 'E', array = [P[1]])
+    col6 = fits.Column(name='u', format = 'E', array = [P[0]])
+#    if (Dec_strip == -26.0 or Dec_strip == -26.7 or Dec_strip == -27.0):
+#        col5 = fits.Column(name='e', format = 'E', array = [polycoeffs[4]])
+#        cols = fits.ColDefs([col1, col2, col3, col4, col5])
+#    else:
+    cols = fits.ColDefs([col1, col2, col3, col4, col5, col6])
+    tbhdu = fits.new_table(cols)
+    tbhdu.writeto(outcoeff, clobber = True)
+
 
 #fitplot.show()
 #fitplot.subplots_adjust(left=sbplt_pad_left, bottom=sbplt_pad_bottom, right=sbplt_pad_right, top=sbplt_pad_top, wspace=sbplt_pad_wspace, hspace=sbplt_pad_hspace)
 if options.make_plots:
-    fitplot.savefig('fits.png',pad_inches=0.0,bbox_inches='tight')
+    outpng=input_root+"_"+"xxyy_int.png"
+    fitplot.savefig(outpng,pad_inches=0.0,bbox_inches='tight')
